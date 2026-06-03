5d9729191b6f0c46628314b0d0003e229a92394c
Takura <300takura@gmail.com>
2026-06-03 21:39:57 +0200

    chore: setup deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://iamtakura.github.io',
+  base: '/VelvetMark',
+  output: 'static'
+});
